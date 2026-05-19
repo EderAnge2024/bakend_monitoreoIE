5b1764175626931f3c5da9975aa6c8e6e9de0a46
ClonNetflix <ederangeloingabauitsta@gmail.com>
2026-05-18 23:22:00 -0500

    final

--- src/app.js
+++ src/app.js
@@ -25,14 +25,16 @@ const documentoRoutes = require('./modules/documentos/documento.routes');
 
 const app = express();
 
-// Middlewares
+// Middlewares - Morgan primero para registrar absolutamente todas las peticiones
+const morganFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+app.use(morgan(morganFormat));
+
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 }));
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
