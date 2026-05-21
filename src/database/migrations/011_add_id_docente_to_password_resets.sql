2d531b79aeaf790ecaa8e829b8308ffc1e6ba3fb
ClonNetflix <ederangeloingabauitsta@gmail.com>
2026-05-20 22:12:04 -0500

    final ahora si

--- src/database/migrations/011_add_id_docente_to_password_resets.sql
+++ src/database/migrations/011_add_id_docente_to_password_resets.sql
@@ -1,1 +1,3 @@
--- Add id_docente column to password_resets\nALTER TABLE password_resets\nADD COLUMN IF NOT EXISTS id_docente BIGINT REFERENCES docentes(id) ON DELETE CASCADE;
+-- Add id_docente column to password_resets
+ALTER TABLE password_resets
+ADD COLUMN IF NOT EXISTS id_docente BIGINT REFERENCES docentes(id_docente) ON DELETE CASCADE;
